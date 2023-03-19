387142fcddcc62996f89ef959503acbc7342498e
Anderson <anderson.asm201@gmail.com>
2023-03-19 14:06:12 -0500

    Vinculado el js con el html y terminada la funcionalidad

---
+++ js/main.js
@@ -0,0 +1,37 @@
+window.addEventListener('load' , ()=>{/* Escuchamos cuando se carga el documneto */
+    const display = document.querySelector('.calculadora-display');
+    const keypadButton = document.getElementsByClassName('keypad-button');
+
+    const keypadButtonArray = Array.from(keypadButton)
+
+    keypadButtonArray.forEach( (button) => {
+        button.addEventListener('click' , () =>{
+            calculadora(button , display)
+        })
+    })
+});
+function calculadora(button, display) {
+    switch(button.innerHTML){
+        case 'C':
+            borrar(display);
+            break;
+        case '=':
+            calcular(display);
+            break;
+        default:
+        actualizar(display , button);
+        break;
+    }
+}
+function calcular(display) {
+    display.innerHTML = eval(display.innerHTML);
+};
+function actualizar(display, button) {
+    if(display.innerHTML == 0){
+        display.innerHTML = " ";
+    }
+    display.innerHTML += button.innerHTML
+}
+function borrar(display){
+    display.innerHTML = 0
+}
